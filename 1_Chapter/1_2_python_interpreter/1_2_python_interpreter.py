b2f1ae5cd79a3d40080fd2bfe44c394e76fa3f42
Serhat <mserhatbalik@gmail.com>
2025-09-14 17:32:47 +0300

    Added python interpreter topic

---
+++ 1_Chapter/1_2_python_interpreter/1_2_python_interpreter.py
@@ -0,0 +1,10 @@
+# We check the confirmation of the installation by running the python interpreter
+# 1. Open the CMD
+# 2. Type python
+# 3. The interpreter should run and print out something similar to the output below.
+# 4. Python 3.13.7 (tags/v3.13.7:bcee1c3, Aug 14 2025, 14:15:11) [MSC v.1944 64 bit (AMD64)] on win32
+#    Type "help", "copyright", "credits" or "license" for more information.
+# 5. Run the command below
+print("Hello Python Interpreter!")
+
+# 7. In most basic cases, we use the python interpreter to run our code on the fly and test it.
