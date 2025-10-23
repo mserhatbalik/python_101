02f1ed908e847e16c07ee370339fc9c429b0570e
Serhat <mserhatbalik@gmail.com>
2025-10-23 17:29:29 +0300

    Added section 2_6

---
+++ 2_Variables_And_Simple_Data_Types/2_6_combine_fstrings_with_methods/2_6_combine_fstrings_with_methods.py
@@ -0,0 +1,7 @@
+# It is also possible to use f-string functionality alongside with string methods
+
+first_name = "serhat"
+last_name = "balık"
+full_name = f"{first_name} {last_name}"
+message = f"Hello, {full_name.title()}!"
+print(message)
