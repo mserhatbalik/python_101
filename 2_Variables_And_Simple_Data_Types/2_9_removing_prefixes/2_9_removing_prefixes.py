da24ad3399adb86e11982085e461ad2177258aa8
Serhat <mserhatbalik@gmail.com>
2025-10-23 18:00:21 +0300

    Added section 2_9

---
+++ 2_Variables_And_Simple_Data_Types/2_9_removing_prefixes/2_9_removing_prefixes.py
@@ -0,0 +1,6 @@
+# In some cases it is necessary to remove a prefix part of a string. For that operation, removeprefix() method is used.
+
+website_url = "https://google.com"
+print(website_url)
+website_url = website_url.removeprefix("https://")
+print(website_url)
