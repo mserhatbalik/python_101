6cd894c26fd0ff16b97455e1b3b9620412e60370
Serhat <mserhatbalik@gmail.com>
2025-10-23 17:40:36 +0300

    Added section 2_7

---
+++ 2_Variables_And_Simple_Data_Types/2_7_adding_whitespace_and_new_lines/2_7_adding_whitespace_and_new_lines.py
@@ -0,0 +1,10 @@
+# We can use whitespaces and new lines to format the console output of our programs.
+
+# Adding white space to strings with tabs
+print("\tpython")
+
+# Creating new lines in strings
+print("Languages:\nPython\nC#\nJava\nKotlin")
+
+# Combining tabs with new lines
+print("Languages:\n\tMy Name is\n\tSerhat Balık")
