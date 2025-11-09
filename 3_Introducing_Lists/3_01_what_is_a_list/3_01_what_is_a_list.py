ecf6253875ecd986c5e7ce2c9a4b59b694209d38
Serhat <mserhatbalik@gmail.com>
2025-11-09 23:44:25 +0300

    Added lists introduction section

---
+++ 3_Introducing_Lists/3_01_what_is_a_list/3_01_what_is_a_list.py
@@ -0,0 +1,5 @@
+# Lists are what we use store multiple data points in order to access them at a later point.
+# We use [] square brackets in order to define a List. We also use (,) comma in order to seperate individual elements of a list.
+
+# names = ["serhat", "ahmet", "mustafa", "lale", "mehtap", "zafer"]
+# print(names)
