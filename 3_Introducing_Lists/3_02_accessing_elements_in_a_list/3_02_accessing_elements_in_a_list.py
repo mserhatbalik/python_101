b6a1e13a5843f43e5fd16fee5a2f4a15ef41592f
Serhat <mserhatbalik@gmail.com>
2025-11-09 23:48:10 +0300

    Added list access section

---
+++ 3_Introducing_Lists/3_02_accessing_elements_in_a_list/3_02_accessing_elements_in_a_list.py
@@ -0,0 +1,5 @@
+# Lists are ordered collections. So we can use the index position of a list in order to access an item inside that list.
+# Remember that in programming, indexes usually start from 0
+
+# car_brands = ["porche", "bmw", "wolkswagen", "audi"]
+# print(car_brands[1])
