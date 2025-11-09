85d9966304a2fa34730f8af112b22000da4a6471
Serhat <mserhatbalik@gmail.com>
2025-11-09 16:38:13 +0300

    Added integers section

---
+++ 2_Variables_And_Simple_Data_Types/2_12_integers/2_12_integers.py
@@ -0,0 +1,38 @@
+# INTEGERS
+# Integers are the simplest form of numerical data representation in Python and programming in general.
+# You can Add (+), Subtract (-), Multiply (*), Divide (/) integers.
+
+# Adding integers
+# num1 = 10
+# num2 = 15
+# sum = num1 + num2
+# print(sum)
+
+
+# Subtracting integers
+# num1 = 5
+# num2 = 10
+# net = num1 - num2
+# print(net)
+
+# Multiplying integers
+# num1 = 23
+# num2 = 100
+# total = num2 * num1
+# print(total)
+
+# Dividing integers
+# num1 = 25
+# num2 = 10
+# divide = num2 / num1
+# print(divide)
+
+# Power integers
+# num1 = 5
+# num2 = 3
+# pow = num1 ** num2
+# print(pow)
+
+# Order of operations
+# net = (3+4) * 5
+# print(net)
