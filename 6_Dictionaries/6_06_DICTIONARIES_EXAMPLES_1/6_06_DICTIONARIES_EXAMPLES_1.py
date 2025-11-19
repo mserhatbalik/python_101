6259af9a5b5e090c1ecd013d9c3ab8ece31edaa0
Serhat <mserhatbalik@gmail.com>
2025-11-19 15:30:15 +0300

    Added first part of dictionary examples

---
+++ 6_Dictionaries/6_06_DICTIONARIES_EXAMPLES_1/6_06_DICTIONARIES_EXAMPLES_1.py
@@ -0,0 +1,50 @@
+"""
+6-1. Person: Use a dictionary to store information about a person you know.
+Store their first name, last name, age, and the city in which they live.
+You should have keys such as first_name, last_name, age, and city. Print each piece of information stored in your dictionary.
+"""
+from pyexpat.errors import messages
+
+# person = {}
+# person["first_name"] = "Serhat"
+# person["last_name"] = "Balık"
+# person["age"] = 40
+# person["city"] = "İstanbul"
+# print(person)
+
+
+
+
+"""
+6-2. Favorite Numbers: Use a dictionary to store people’s favorite numbers. Think of five names, and use them as keys in your dictionary. 
+Think of a favorite number for each person, and store each as a value in your dictionary. Print each person’s name and their favorite number. 
+For even more fun, poll a few friends and get some actual data for your program.
+"""
+# favorite_numbers = {"jack": 3, "janice": 0, "adam": 25, "ilana": 11, "robert": 13}
+# print(f"jack's favorite number is {favorite_numbers["jack"]}")
+# print(f"janice's favorite number is {favorite_numbers["janice"]}")
+# print(f"robert's favorite number is {favorite_numbers["robert"]}")
+# print(f"ilana's favorite number is {favorite_numbers["ilana"]}")
+# print(f"adam's favorite number is {favorite_numbers["adam"]}")
+
+
+"""
+6-3. Glossary: A Python dictionary can be used to model an actual dictionary. However, to avoid confusion, let’s call it a glossary.
+
+Think of five programming words you’ve learned about in the previous chapters. 
+Use these words as the keys in your glossary, and store their meanings as values.
+Print each word and its meaning as neatly formatted output. 
+You might print the word followed by a colon and then its meaning, or print the word on one line and then print its meaning indented on a second line. 
+Use the newline character (\n) to insert a blank line between each word-meaning pair in your output.
+"""
+
+# programming_words = {"integer": "A numerical representation of a value",
+#                      "slice": "A function to copy a part of a list",
+#                      "list": "A container which stores arbitrary amount of data",
+#                      "for-loop": "A programming concept that allows us run X amounts of iterations for particular code blocks",
+#                      "if block": "A programming concept that allows us check for particular condition before executing specific code blocks"}
+# print(f"If Blocks: {programming_words.get("if block")}\n")
+# print(f"Integer: {programming_words.get("integer")}\n")
+# print(f"Slice: {programming_words.get("slice")}\n")
+# print(f"For Loops: {programming_words.get("for-loop")}\n")
+# print(f"List: {programming_words.get("list")}\n")
