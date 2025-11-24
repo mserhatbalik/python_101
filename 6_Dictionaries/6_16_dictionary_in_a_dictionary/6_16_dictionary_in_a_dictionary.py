35ddfd2c44c96168fdcae0e967d085a5664f9dbb
Serhat <mserhatbalik@gmail.com>
2025-11-24 15:28:35 +0300

    Created additional dictionary sections

--- 6_Dictionaries/6_16_dictionary_in_a_dictionary/6_16_dictionary_in_a_dictionary.py
+++ 6_Dictionaries/6_16_dictionary_in_a_dictionary/6_16_dictionary_in_a_dictionary.py
@@ -1,1 +1,15 @@
-# TODO
+# We can use dictionaries inside dictionaries to represent complex data, but it can also get complicated.
+# In the example below, "username" acts like an identifier; this can also be a numerical identification number to reflect a more realistic scenario.
+# The secondary dictionary defined inside the first one contains the details of that particular user.
+
+users = {"serhatbalik": {
+    "first_name": "serhat", "last_name": "balık", "email": "sbalik@gmail.com"},
+    "yasarkurt": {
+        "first_name": "yaşar", "last_name": "kurt", "email": "ykurt@gmail.com"
+    }}
+
+for username, details in users.items():
+    print(f"Username: {username}")
+    print(f"\tFirst Name: {details["first_name"].title()}")
+    print(f"\tLast Name: {details["last_name"].title()}")
+    print(f"\tEmail: {details["email"]}")
