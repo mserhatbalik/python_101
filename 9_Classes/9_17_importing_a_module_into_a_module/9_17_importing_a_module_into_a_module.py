98d4adee41418057c302f173a0124c82006f7961
Serhat <mserhatbalik@gmail.com>
2025-12-13 13:30:00 +0300

    Added new sections of classes for gemini help

---
+++ 9_Classes/9_17_importing_a_module_into_a_module/9_17_importing_a_module_into_a_module.py
@@ -0,0 +1,7 @@
+import electric_car
+
+my_car = electric_car.Car("Porsche", "911 Turbo S", "2026")
+her_car = electric_car.ElectricCar("BYD", "Seal U DM-i", "2026")
+
+my_car.get_descriptive_name()
+# her_car.battery.describe_battery()
