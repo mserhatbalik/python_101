fd6c0de926f765aa1bf5e38711daa0576aaaaef1
Serhat <mserhatbalik@gmail.com>
2025-12-21 13:23:31 +0300

    Integrated ship class to main file and spawned the ship.

--- 12_Alien_Invasion/alien_invasion.py
+++ 12_Alien_Invasion/alien_invasion.py
@@ -1,6 +1,7 @@
 import sys
 import pygame
 from settings import Settings
+from ship import Ship
 
 class AlienInvasion:
     """Overall class to manage game assets and behavior"""
@@ -16,6 +17,9 @@ class AlienInvasion:
         self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
 
+        # We pass the (self) as the argument to Ship class, where we use all the properties of AlienInvasion class with ai_game argument in Ship class.
+        self.ship = Ship(self)
+
     def run_game(self):
         """Start the main loop for the game."""
         while True:
@@ -26,6 +30,7 @@ class AlienInvasion:
 
             # Redraw the screen during each pass through the loop
             self.screen.fill(self.settings.bg_color)
+            self.ship.blitme()
 
             # Make the most recently drawn screen visible.
             pygame.display.flip()
@@ -38,3 +43,7 @@ if __name__ == '__main__':
     # Make a game instance and run the game.
     ai = AlienInvasion()
     ai.run_game()
+
+
+# TODO
+# Refactoring: The _check_events() and _update_screen() Methods
