57dbeeb25b4b281877c2689cf79d5ff2bc55291e
Serhat <mserhatbalik@gmail.com>
2025-12-22 21:25:07 +0300

    Added both ways ship moivng, but bugged currently. Asking gemini.

--- 12_Alien_Invasion/alien_invasion.py
+++ 12_Alien_Invasion/alien_invasion.py
@@ -25,6 +25,7 @@ class AlienInvasion:
         """Start the main loop for the game."""
         while True:
             self._check_events()
+            self.ship.update()
             self._update_screen()
             # Set the framerate to 60. It effectively sets a "sleep" time to compensate for fast computers. It waits if the code above in the loop executes fast.
             self.clock.tick(60)
@@ -38,8 +39,16 @@ class AlienInvasion:
                 sys.exit()
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_RIGHT:
-                    self.ship.rect.x += 1
-
+                    self.ship.moving_right = True
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    self.ship.moving_left = True
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = False
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_LEFT:
+                    self.ship.moving_left = False
 
     def _update_screen(self):
         # Redraw the screen during each pass through the loop
