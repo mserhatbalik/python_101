b82f2acdb1975fcd15daa5d433f122589f8f3857
Serhat <mserhatbalik@gmail.com>
2026-02-02 16:19:07 +0300

    Implemented Die class and frequency detection through multiple rolls.

---
+++ 15_Generating_Data/15_15_rolling_dice_with_plotly/15_15_rolling_dice_with_plotly.py
@@ -0,0 +1,38 @@
+from random import randint
+
+class Die:
+    """A class representing a single die."""
+    def __init__(self, num_sides = 6):
+        """Assume a six sided dice"""
+        self.num_sides = num_sides
+
+    def roll(self):
+        """Return a random value between 1 and number of sides"""
+        return randint(1, self.num_sides)
+
+# Create D6
+die = Die()
+
+# Make some rolls and return a list
+results = []
+for roll_num in range(1000):
+    result = die.roll()
+    results.append(result)
+
+# ANALYZE THE RESULTS
+# Define the empty list to store total frequencies of each die roll
+frequencies = []
+
+# Define the total frequency range to loop thorugh. In this instance default num_sides is 6. range() function works through 1 to 7, but excluding 7.
+poss_results = range(1,die.num_sides+1)
+
+# Iterate through all frequencies. Start with 1.
+for value in poss_results:
+    # Count all the 1s in the results, then 2, then 3, etc. in each loop
+    frequency = results.count(value)
+
+    # Append the occurance of each side to the frequencies list.
+    frequencies.append(frequency)
+
+# The results format is [151, 168, 167, 172, 180] as an example. 151 is how many 1s are in the results list, 168 is how many 2s are in the results list, etc.
+print(frequencies)
